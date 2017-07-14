5c5c750f2f18bac6aba703164d16892bf7aa4544
dorkblue <d.dorkblue@gmail.com>
2017-07-14 18:36:59 +0800

    added invoice create

--- controllers/invoiceController.js
+++ controllers/invoiceController.js
@@ -1,5 +1,17 @@
+const InvoiceModel = require('../models/Invoice')
+
 module.exports = {
   create: (req, res, next) => {
     console.log(req.body)
+
+    const newInvoice = new InvoiceModel(req.body)
+
+    newInvoice.save((err, saved) => {
+      if (err) {
+        res.json(err)
+      } else {
+        res.json(saved)
+      }
+    })
   }
 }
--- models/Invoice.js
+++ models/Invoice.js
@@ -1,7 +1,23 @@
 const mongoose = require('mongoose')
 
 const invoiceObj = {
+  transactions: [{
+    transaction: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Transaction'
+    },
+    receivable: {
+      percentage: {
+        type: Number
+      },
+      amount: {
+        type: Number
+      }
+    },
+    addons: {
 
+    }
+  }]
 }
 
 const invoiceSchema = new mongoose.Schema(invoiceObj)
