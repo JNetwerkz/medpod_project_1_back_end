773b61ce6b409aba8718b980d0cdba7c81bd5b54
dorkblue <d.dorkblue@gmail.com>
2017-08-15 11:38:46 +0800

    added operator to check if in development at firebase.js

--- firebase.js
+++ firebase.js
@@ -5,7 +5,7 @@ const clientAdmin = admin.initializeApp({
     projectId: process.env.GOOGLE_ADMIN_PROJECT_ID,
     clientEmail: process.env.GOOGLE_ADMIN_CLIENT_EMAIL,
     // privateKey: JSON.parse(process.env.GOOGLE_ADMIN_PRIVATE_KEY)
-    privateKey: process.env.GOOGLE_ADMIN_PRIVATE_KEY
+    privateKey: !process.env.NODE_ENV || process.env.NODE_ENV === 'development' ? process.env.GOOGLE_ADMIN_PRIVATE_KEY : JSON.parse(process.env.GOOGLE_ADMIN_PRIVATE_KEY)
   })
 })
 
